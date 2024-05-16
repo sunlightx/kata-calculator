3a5cf67b472cce7be3eb52da5792feb54ff0b683
unknown <sliepkov.1998@gmail.com>
2024-05-16 15:19:41 +0300

    update

--- main.go
+++ main.go
@@ -23,16 +23,25 @@ func main() {
 	}
 
 	intToRoman := map[int]string{
-		1:  "I",
-		2:  "II",
-		3:  "III",
-		4:  "IV",
-		5:  "V",
-		6:  "VI",
-		7:  "VII",
-		8:  "VIII",
-		9:  "IX",
-		10: "X",
+		1:   "I",
+		2:   "II",
+		3:   "III",
+		4:   "IV",
+		5:   "V",
+		6:   "VI",
+		7:   "VII",
+		8:   "VIII",
+		9:   "IX",
+		10:  "X",
+		20:  "XX",
+		30:  "XXX",
+		40:  "XL",
+		50:  "L",
+		60:  "LX",
+		70:  "LXX",
+		80:  "LXXX",
+		90:  "XC",
+		100: "C",
 	}
 
 	reader := bufio.NewReader(os.Stdin)
@@ -119,20 +128,19 @@ func main() {
 	}
 }
 
-func arabicToRoman(num int, romanToInt map[int]string) string {
-	var roman string
+func arabicToRoman(num int, intToRoman map[int]string) string {
+	// Check if the number is directly available in the map
+	if roman, exists := intToRoman[num]; exists {
+		return roman
+	}
 
-	if num >= 10 {
-		tens := num / 10
-		for i := 0; i < tens; i++ {
-			roman += romanToInt[10]
+	roman := ""
+	values := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
+	for _, value := range values {
+		for num >= value {
+			roman += intToRoman[value]
+			num -= value
 		}
-		num = num % 10
 	}
-
-	if num > 0 {
-		roman += romanToInt[num]
-	}
-
 	return roman
 }
